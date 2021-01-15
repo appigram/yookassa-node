7aaf74e86a6090e08282247c01356672c531514f
yuriyvorobyov96 <darthrevan1333@gmail.com>
2021-01-15 09:32:07 +0300

    linted index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 'use strict';
 
-var init = require('./lib/index');
+const init = require('./lib/index');
+
 module.exports = init;
-// expose constructor as a named property to enable mocking with Sinon.JS
+
+// Expose constructor as a named property to enable mocking with Sinon.JS
 module.exports.YandexCheckout = init.YandexCheckout;
